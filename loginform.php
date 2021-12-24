a3db0778f32d596c04f7f75e59d04c96198b73dc
Abdelrahman Elewa <95569208+AbdelrahmanElewaa@users.noreply.github.com>
2021-12-25 00:57:06 +0200

    Add files via upload

--- loginform.php
+++ loginform.php
@@ -9,6 +9,8 @@
      <link rel="stylesheet" href="css/bootstrap.min.css">
      <link rel="stylesheet" href="css/all.min.css">
      <link rel="stylesheet" href="css/loginform.css">
+     
+     <script src="js/validation.js"></script>
      <?php
      session_start();
      ?>
@@ -17,21 +19,67 @@
 
  <div class="container border">
    <h3 class="text-center">Login</h3>
-                <form class="form-group" action="#" method="post" onsubmit="return validateform()">
+                <form class="form-group" action="#" method="post" onsubmit="return validate()">
               Email <input class="form-control" type="email" autocomplete="off" placeholder="Email" name="email" id="emailID"><br>
              <div id="emailerror">
 
              </div>
-              Password<input class="form-control" type="password" placeholder="Password" name="password" id="passwordID">
-              <div id="passworderror"> </div>
+              Password
+              <input class="form-control" type="password" autocomplete="off" placeholder="Password" name="password" id="passwordID">
+              <div id="passworderror"> 
+
+              </div>
               <input type="submit" id="loginm" name="loginsubmit" class="btn btn-primary loginmodal" value="Log in">
               
             </form>
             </div>
+<script>
+ 
+function validate()
+{
+    var user_email = document.getElementById('emailID').value ;
+    var user_pass = document.getElementById('passwordID').value;
 
+  if(user_email == '' || user_pass ==''){
+       
+      if(user_email == ''){
+            document.getElementById('emailerror').innerHTML = 'Email is Required';
+            
+        } 
+        else{
+          document.getElementById('emailerror').innerHTML='';
+        }
+        
+
+        if(user_pass =='')
+        {
+            document.getElementById('passworderror').innerHTML = 'Password is Required';
+        }
+        else{
+          document.getElementById('passworderror').innerHTML='';
+        }
+        
+        return false;
+    
+    }else{
+        return true
+    }
+}
+
+
+
+</script>
             <?php
  
 if(isset($_POST['loginsubmit']))
+{
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+  $sanitizedEmail=filter_var($email,FILTER_SANITIZE_EMAIL);
+	$sanitizedPass=filter_var($password,FILTER_SANITIZE_STRING);
+
+if(filter_var($sanitizedEmail,FILTER_VALIDATE_EMAIL))
 {
 $conn= new mysqli("localhost","root","","eshtrely");
 $sql="SELECT * FROM users WHERE email='".$_POST['email']."' and password='".$_POST['password']."'";
@@ -54,15 +102,24 @@ if($row["role"]=="admin")
 else{
   header("Location:index.php");
 }
+
 }
 else{
 echo"error";
 }
 
 }
-?>
+else{
+  echo "Please enter your Email correctly";
+}
 
-<script src="js/validation.js"></script>
+
+}
+?>
+<!-- <script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/all.min.js"></script> -->
+
  </body>
  </html>
 
--- signupForm.php
+++ signupForm.php
@@ -15,19 +15,104 @@
     <!-- <span class="border border-success"></span> -->
 <div class="container border border-success formcontainer text-center">
     <h3><b> Create Your Account</b></h3>
-    <form  action="" method="post" class="form-group f" enctype="multipart/form-data">
+    <form  action="" method="post" class="form-group f" enctype="multipart/form-data" onsubmit="validate()">
         
-<input type="text" placeholder="Full Name" name="name" class="form-control" id="firstinput">
-<input type="email" placeholder="Email Address" name="email" class="form-control">
-<input type="text" placeholder="Phone Number" name="phone" class="form-control">
-<input type="text" placeholder="Address" name="address" class="form-control">
-<input type="password" placeholder="Password" name="password" class="form-control"><br>
+<input type="text" placeholder="Full Name" name="name" class="form-control" id="name">
+<div id="nameerror"></div>
+<input type="email" placeholder="Email Address" name="email" class="form-control" id="email">
+<div id="emailerror"></div>
+<input type="text" placeholder="Phone Number" name="phone" class="form-control" id="phone">
+<div id="phoneerror"></div>
+<input type="text" placeholder="Address" name="address" class="form-control"id="address">
+<div id="addresserror"></div>
+
+<input type="password" placeholder="Password" name="password" class="form-control" id="password"><br>
+<div id="passworderror"></div>
 <b id="uploadtext">Upload your profile photo:<b><br>
 <input type="file" name="photo"  class="ProfilePhoto"><br>
 <input type="hidden" value="customer" name="role" >
+<input type="submit" value="Sign Up" name="signup" class="btn btn-success signupbtn" class="form-control">
+    </form>
+</div>
+</body>
+
+
+<script>
+function validate()
+{
+    var user_name=document.getElementById('name').value;
+    var user_email = document.getElementById('email').value ;
+    var user_phone=document.getElementById('phone').value;
+    var user_address=document.getElementById('address').value;
+    var user_pass = document.getElementById('password').value;
+    <?php $error=false; ?>
+  if(user_name == '' || user_email =='' || user_phone ==''|| user_address ==''|| user_pass ==''){
+    <?php $error=true; ?>
+        if(user_name == ''){
+            document.getElementById('nameerror').innerHTML = 'Full Name is Required';
+            
+        } 
+        else{
+          document.getElementById('nameerror').innerHTML='';
+        }
+      
+        if(user_email == ''){
+            document.getElementById('emailerror').innerHTML = 'Email is Required';
+            
+        } 
+        else{
+          document.getElementById('emailerror').innerHTML='';
+        }
+      
+
+        if(user_phone == ''){
+            document.getElementById('phoneerror').innerHTML = 'Phone is Required';
+            
+        } 
+        else{
+          document.getElementById('phoneerror').innerHTML='';
+        }
+      
+
+    
+      if(user_address == ''){
+            document.getElementById('addresserror').innerHTML = 'Address is Required';
+            
+        } 
+        else{
+          document.getElementById('addresserror').innerHTML='';
+        }
+        
+
+        if(user_pass =='')
+        {
+            document.getElementById('passworderror').innerHTML = 'Password is Required';
+        }
+        else{
+          document.getElementById('passworderror').innerHTML='';
+        }
+        
+        return false;
+    
+    }else{
+       
+        return true;
+    }
+}
+
+
+
+</script>
 
 <?php
 if(isset($_POST['signup'])){
+    if($error)
+    {
+?>
+<script> validate();</script>
+<?php
+    }
+    else{
 $conn= new mysqli("localhost","root","","eshtrely");
  
 $sql= "INSERT INTO users (name,email,phone,address,password,role,photo) VALUES ('".$_POST['name']."','".$_POST['email']."','".$_POST['phone']."','".$_POST['address']."','".$_POST['password']."','".$_POST['role']."','".$_FILES['photo']['name']."')";
@@ -59,13 +144,13 @@ else{
    }
      header("Location:index.php");
  }
+ else
+ {
+     echo "error";
+ }
 }
-
+}
 ?>
-<input type="submit" value="Sign Up" name="signup" class="btn btn-success signupbtn" class="form-control">
-    </form>
-</div>
-</body>
 
 
 
