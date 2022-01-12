f35f32b68cb64cf0c884ba5cfa59cc17dcc6f370
Ghourab <94136734+Ghourab@users.noreply.github.com>
2022-01-12 23:03:52 +0200

    Add files via upload

---
+++ eshtrely (2).sql
@@ -0,0 +1,255 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 12, 2022 at 10:03 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `eshtrely`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `adminID` int(11) NOT NULL,
+  `adminName` varchar(200) NOT NULL,
+  `adminEmail` varchar(200) NOT NULL,
+  `adminPassword` varchar(100) NOT NULL,
+  `adminPhoto` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `userid` int(55) NOT NULL,
+  `productid` int(55) NOT NULL,
+  `quantity` int(55) NOT NULL,
+  `orderid` int(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`userid`, `productid`, `quantity`, `orderid`) VALUES
+(29, 10, 1, 1),
+(29, 10, 3, 2),
+(29, 11, 1, 3),
+(29, 10, 2, 4),
+(29, 11, 1, 5),
+(29, 10, 1, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `productname` varchar(50) NOT NULL,
+  `productid` varchar(50) NOT NULL,
+  `productprice` double(10,2) NOT NULL,
+  `productimage` varchar(50) NOT NULL,
+  `rating` double(2,1) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `producttype` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`productname`, `productid`, `productprice`, `productimage`, `rating`, `quantity`, `producttype`) VALUES
+('Laptop Dell', '1', 400.00, 'dell.jpg', 0.0, 46, 'electronics'),
+('KitKat Chocolate', '10', 4.00, 'kitkat.jpg', 0.0, 98, 'food'),
+('Pepsi Can', '11', 5.00, 'pepsican.jpg', 0.0, 221, 'food'),
+('Coca Cola 2L Bottle', '12', 10.00, 'coke2l.jpg', 0.0, 148, 'food'),
+('Snickers', '13', 4.00, 'snickers.jpg', 0.0, 197, 'food'),
+('Pepsi 2L Bottle', '14', 10.00, 'pepsi2l.jpeg', 0.0, 150, 'food'),
+('Chile Limon Chips', '15', 15.00, 'chilelimonchips.jpeg', 0.0, 199, 'food'),
+('Liverpool Nike T-Shirt', '16', 200.00, 'liverpoolshirt.jpg', 0.0, 146, 'fashion'),
+('Black Nike T-Shirt', '17', 250.00, 'nikeblackshirt.jpg', 0.0, 149, 'fashion'),
+('Nike Black Sweatpants', '18', 150.00, 'nikeblackpants.jpg', 0.0, 239, 'fashion'),
+('Nike Green Sweatpants', '19', 150.00, 'nikegreenpants.jpg', 0.0, 199, 'fashion'),
+('Nike Shoes', '2', 200.00, 'nikeshoe.jpg', 0.0, 19, 'fashion'),
+('Tomato Chips', '3', 15.00, 'tomatoochips.jpg', 0.0, 100, 'food'),
+('Twinkies', '4', 2.00, 'twinkies.jpg', 0.0, 500, 'food'),
+('Galaxy Chocolate', '5', 4.00, 'galaxy.jpg', 0.0, 300, 'food'),
+('IPhone 13', '6', 699.00, 'iPhone13.jpg', 0.0, 198, 'electronics'),
+('Rice Bag', '7', 10.00, 'rice.jpg', 0.0, 100, 'food'),
+('Sour Cream & Onions Chips', '8', 15.00, 'sourcreamchips.jpg', 0.0, 200, 'food'),
+('Lenovo A5s', '9', 300.00, 'LenovoA5s.jpeg', 0.0, 299, 'electronics');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rating`
+--
+
+CREATE TABLE `rating` (
+  `userid` int(55) NOT NULL,
+  `productid` int(55) NOT NULL,
+  `productrating` double(1,1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `rating`
+--
+
+INSERT INTO `rating` (`userid`, `productid`, `productrating`) VALUES
+(29, 10, 0.0),
+(29, 10, 0.0),
+(29, 10, 0.0),
+(0, 1, 0.0),
+(0, 1, 0.0),
+(0, 1, 0.0),
+(29, 1, 0.0),
+(29, 1, 0.0),
+(29, 1, 0.0),
+(29, 1, 0.0),
+(29, 1, 0.0),
+(29, 1, 0.0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `reviewid` int(100) NOT NULL,
+  `userid` int(55) NOT NULL,
+  `username` varchar(55) NOT NULL,
+  `userimage` varchar(55) NOT NULL,
+  `productid` int(100) NOT NULL,
+  `reviewdate` varchar(100) NOT NULL,
+  `review` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`reviewid`, `userid`, `username`, `userimage`, `productid`, `reviewdate`, `review`) VALUES
+(9, 29, 'Ahmed Sameh', 'photo-1617127365659-c47fa864d8bc.jfif', 11, 'Sunday 9th of January 2022 02:08:53 PM', 'ent3a4'),
+(11, 30, 'shehab', 'images.jfif', 11, 'Sunday 9th of January 2022 02:23:16 PM', 'refreshing'),
+(12, 29, 'Ahmed Sameh', 'photo-1617127365659-c47fa864d8bc.jfif', 1, 'Wednesday 12th of January 2022 09:38:11 PM', 'nice');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `photo` varchar(200) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `address`, `role`, `photo`, `created_at`, `updated_at`) VALUES
+(14, 'Amr Elewa', 'amrelewa@gmail.com', '123456', '01002585821', 'nasr city', 'admin', 'WhatsApp Image 2021-12-13 at 5.56.22 PM.jpeg', NULL, NULL),
+(21, 'Ali Khaled', 'ali@email.com', '218', '01140414492', '85 haroun elrashid- Misr elgdeda', 'customer', 'WhatsApp Image 2021-12-13 at 6.03.59 PM.jpeg', NULL, NULL),
+(24, 'Hady Wael ', 'hadywk@gmail.com', '1234', '01151665100', 'nasr city', 'customer', 'background.jpg', NULL, NULL),
+(28, 'mohmed', 'Mazenwk@gmail.com', '9876', '0309393', 'dehk st', 'customer', 'the_child_star_wars_gallery_5e3204be4f668.jpg', NULL, NULL),
+(29, 'Ahmed Sameh', 'Ghourab@gmail.com', '682001', '01015266031', 'ahmed el sawy- nasr city, building 50-apartment11', 'customer', 'photo-1617127365659-c47fa864d8bc.jfif', NULL, NULL),
+(30, 'shehab', 'shehab@gmail.com', '123', '123', 'shrouk', 'customer', 'images.jfif', NULL, NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`adminID`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderid`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`productid`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`reviewid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `adminID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `orderid` int(55) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `reviewid` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
