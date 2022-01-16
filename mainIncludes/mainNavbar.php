219be422f4e553c5c3fff80250383aa8bbfa9efc
Shehab ElDin Mohsen Hamed <65345727+ShehabEMohsen@users.noreply.github.com>
2022-01-16 18:51:35 +0200

    Add files via upload

--- mainIncludes/mainNavbar.php
+++ mainIncludes/mainNavbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="index.php">Eshtrely</a>
+        <a class="navbar-brand" href="index.php" style="font-size: 35px; font-weight: 600;">Eshtrely</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -7,9 +7,6 @@
           <ul class="navbar-nav">
             <li class="nav-item active">
               <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Daily deals</a>
             </li>
             
             <li class="nav-item dropdown">
