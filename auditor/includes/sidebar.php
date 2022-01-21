3e7ca1fa2054a4f5ca846da0b117b37a37c44040
Abdelrahman Elewa <95569208+AbdelrahmanElewaa@users.noreply.github.com>
2022-01-21 11:17:58 +0200

    Add files via upload

--- auditor/includes/sidebar.php
+++ auditor/includes/sidebar.php
@@ -77,6 +77,12 @@ if(isset($_POST['logout']))
                   <p>Admins</p>
                 </a>
               </li>
+              <li class="nav-item">
+                <a href="userSurvey.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Surveys</p>
+                </a>
+              </li>
             
              
         </ul>
